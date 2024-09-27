409fe43d618fdb35e2898f8ac7dc805540b7eadc
Roei <roei.benporat@gmail.com>
2024-09-27 16:38:54 -0500

    Added a second button

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let counter = 0;
+let counter2 = 0;
 document.getElementById('clickMe').addEventListener('click', function() {
-    counter++;
+    counter += 2;
     document.getElementById('message').textContent = `Button clicked ${counter} times!`;
+});
+document.getElementById('clickMe2').addEventListener('click', function() {
+    counter2++;
+    document.getElementById('message2').textContent = `Button clicked ${counter2} times`;
 });
